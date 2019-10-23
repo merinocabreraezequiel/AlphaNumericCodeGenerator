ae700450540636bdef775765dbd4e8f9ca5734a3
Ezequiel Merino <emerino@aistechnology.es>
2019-10-23 11:17:03 +0200

    first Commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# AlphaNumericCodeGenerator
+
+Sencillo proyecto en `python 3` para generar secuencialmente todas las combinaciones de letras y numeros.
+Inicialmente pensado para saber cuanto se tardaría en realizar esta acción y calcular el porcentage que representa solo la parte numérica
+
+### Pre-requisitos 📋
+
+python 3.7 [Download](https://www.python.org/downloads/release/python-375/)
+
+
+### Instalación 🔧
+
+Clona o descarga el repositorio
+
+Accede a la carpeta del proyecto y ejecuta 
+```
+python combinatoria.py
+```
+
+## Construido con 🛠️
+
+* [Python](https://www.python.org/downloads/release/python-375/) - Operativo en cualquier arquitectura soportada
+
+
+## Autores ✒️
+
+* **Ezequiel Merino** - *Desarrollo y documentación* - [github](https://github.com/merinocabreraezequiel)
+
+## Licencia 📄
+
+Este proyecto está bajo la Licencia (GPL3) - mira el archivo [LICENSE.md](LICENSE.md) para detalles
+
+## Agradecimientos 🎁
+
+* Dr. Ricardo Zarzuela, por las consultas sobre la aproximación matemática de los valores con los que trabajamos
+
---
+++ combinatoria.py
@@ -0,0 +1,39 @@
+import string
+import time
+
+def stepbystep(characters = [0,0,0,0,0,0,0,0,0,0,0,0]):
+	lettersAndDigits = string.ascii_letters + string.digits
+	return ''.join(lettersAndDigits[characters[letterPosition]] for letterPosition in range(12))
+
+def dayshoursminutes():
+	totalSeconds = round(time.time() - start_time)
+	day = totalSeconds // (24 * 3600)
+	totalSeconds = totalSeconds % (24 * 3600)
+	hour = totalSeconds // 3600
+	totalSeconds %= 3600
+	minutes = totalSeconds // 60
+	totalSeconds %= 60
+	seconds = totalSeconds
+	dhms = str(day) + ":" + str(hour) + ":" + str(minutes) + ":" + str(seconds)
+	return (dhms)
+
+start_time = time.time()
+count=0
+onlyDigit=0
+for a in range(62):
+	for b in range(62):
+		for c in range(62):
+			for d in range(62):
+				for e in range(62):
+					for f in range(62):
+						for g in range(62):
+							for h in range(62):
+								for i in range(62):
+									for j in range(62):
+										for k in range(62):
+											for l in range(62):
+												count += 1
+												code = stepbystep([a,b,c,d,e,f,g,h,i,j,k,l])
+												if (code.isdigit()):
+													onlyDigit += 1
+												print (onlyDigit, "/", count, "(", dayshoursminutes(), ") -", code)
